6e8cd0454284383c5732148e8b21246267dfeb4b
Pedro Kunz <pedrokunz094@gmail.com>
2025-05-30 20:41:58 -0300

    change huffman methods to static

--- src/main/java/com/documents/management/system/infrastructure/algorithms/Huffman.java
+++ src/main/java/com/documents/management/system/infrastructure/algorithms/Huffman.java
@@ -1,11 +1,11 @@
 package com.documents.management.system.infrastructure.algorithms;
 
 public class Huffman {
-    public String compress(String data) {
+    public static String compress(String data) {
         return "";
     }
 
-    public String decode(String encodedData) {
+    public static String decode(String encodedData) {
         return "";
     }
 }
