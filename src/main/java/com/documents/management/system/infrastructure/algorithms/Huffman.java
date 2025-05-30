b5f71a403a4ebd275c3c453892a8f56cbeadcae9
Pedro Kunz <pedrokunz094@gmail.com>
2025-05-30 19:48:17 -0300

    create huffman class definition

---
+++ src/main/java/com/documents/management/system/infrastructure/algorithms/Huffman.java
@@ -0,0 +1,11 @@
+package com.documents.management.system.infrastructure.algorithms;
+
+public class Huffman {
+    public String compress(String data) {
+        return "";
+    }
+
+    public String decode(String encodedData) {
+        return "";
+    }
+}
