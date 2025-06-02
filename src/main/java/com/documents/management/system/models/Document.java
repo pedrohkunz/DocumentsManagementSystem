8ec68b8950e68f943dea8dc53e0ff91429c32eda
Pedro Kunz <pedrokunz094@gmail.com>
2025-06-02 17:04:04 -0300

    adding lombok dependency to project

--- src/main/java/com/documents/management/system/models/Document.java
+++ src/main/java/com/documents/management/system/models/Document.java
@@ -1,4 +1,11 @@
 package com.documents.management.system.models;
 
 public class Document {
+    private String title;
+    private String content;
+
+    public Document(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
 }
