bb91f3d3628ebd16d94082c8d6941e2a323149a5
Vinícius Roulim Gonçalves <viniciusrg2003@gmail.com>
2025-06-05 16:44:35 -0300

    criado o algorítimo de ordenação alfabética e a classe quickSearch

---
+++ src/main/java/com/documents/management/system/engine/quickSearch/quickSearch.java
@@ -0,0 +1,6 @@
+package com.documents.management.system.engine.quickSearch;
+
+public class quickSearch {
+    // deve chamar o .map para percorrer pelos arquivos e armazenar as palavras em um array
+    // depois deve chamar o BubbleSortAlphabetical.bubbleSort(passando esse parâmetro do array)
+}
