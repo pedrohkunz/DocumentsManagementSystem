2d86231df454860cd6e0a220c42d26fb3f54649b
Pedro Kunz <pedrokunz094@gmail.com>
2025-06-16 17:54:01 -0300

    feat: create method that return the best algorithm for list

--- src/main/java/com/documents/management/system/engine/factories/SortAlgorithmFactory.java
+++ src/main/java/com/documents/management/system/engine/factories/SortAlgorithmFactory.java
@@ -4,6 +4,8 @@ import com.documents.management.system.engine.algorithms.HeapSort;
 import com.documents.management.system.engine.algorithms.MergeSort;
 import com.documents.management.system.engine.algorithms.QuickSort;
 import com.documents.management.system.engine.algorithms.SelectionSort;
+import com.documents.management.system.engine.structures.CustomLinkedList;
+import com.documents.management.system.models.Document;
 
 public class SortAlgorithmFactory {
     public static SortAlgorithmInterface createSortAlgorithm(String algorithmType) {
@@ -19,5 +21,53 @@ public class SortAlgorithmFactory {
             default:
                 throw new IllegalArgumentException("Unknown sorting algorithm type: " + algorithmType);
         }
+    }
+
+    public static SortAlgorithmInterface createOptimizedSortAlgorithm(CustomLinkedList<Document> list) {
+        if (list.size() <= 10) {
+            return new SelectionSort();
+        }
+
+        if (isPartiallySorted(list) || hasManyDuplicates(list)) {
+            return new MergeSort();
+        }
+
+        if (list.size() > 1000) {
+            return new HeapSort();
+        }
+
+        return new QuickSort();
+    }
+
+    private static boolean isPartiallySorted(CustomLinkedList<Document> list) {
+        if (list.size() < 2) return false;
+
+        int sortedCount = 0;
+        for (int i = 0; i < list.size() - 1; i++) {
+            if (list.get(i).getTitle().compareTo(list.get(i + 1).getTitle()) <= 0) {
+                sortedCount++;
+            }
+        }
+        
+        return sortedCount >= list.size() / 2;
+    }
+
+    private static boolean hasManyDuplicates(CustomLinkedList<Document> list) {
+        if (list.size() < 2) return false;
+
+        int duplicateCount = 0;
+        for (int i = 0; i < list.size(); i++) {
+            Document current = list.get(i);
+            for (int j = i + 1; j < list.size(); j++) {
+                if (current.getTitle().equals(list.get(j).getTitle())) {
+                    duplicateCount++;
+                    if (duplicateCount > list.size() / 10) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
     }
 }
